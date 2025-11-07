dda2d81b207d878920ee0f8478159bf99051a338
RaphiVoe <raphael.voetterl.94@gmail.com>
2025-11-07 21:09:06 +0100

    Add CLI support, improve book analysis, and sort character counts

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from stats import get_num_words, get_char_counts
+import sys
+from stats import (
+    get_num_words,
+    get_char_counts,
+    get_sorted_list_of_dicts
+)
 
 
 def get_book_text(filepath):
@@ -9,11 +14,28 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    print("============ BOOKBOT ============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    bookpath = sys.argv[1]
+
+    print(f"Analyzing book found at {bookpath}...")
+    book_text = get_book_text(bookpath)
+
+    print("----------- Word Count ----------")
     num_words = get_num_words(book_text)
-    char_counts = get_char_counts(book_text)
     print(f"Found {num_words} total words.")
-    print(char_counts)
+
+    print("--------- Character Count -------")
+    char_counts = get_char_counts(book_text)
+    sorted_char_counts = get_sorted_list_of_dicts(char_counts)
+    for char_count in sorted_char_counts:
+        if not char_count["char"].isalpha():
+            continue
+        print(f"{char_count['char']}: {char_count['num']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,18 @@ def get_char_counts(string):
         else:
             counts[char] = 1
     return counts
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def get_sorted_list_of_dicts(char_dict):
+    chars = []
+    for char in char_dict:
+        chars.append({
+            'char': char,
+            'num': char_dict[char]
+        })
+    chars.sort(reverse=True, key=sort_on)
+    return chars
